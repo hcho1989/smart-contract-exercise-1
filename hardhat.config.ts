852c8af038ea5b5f042b842562948d11a300fda1
Ho Chi Ho <hcho1989@gmail.com>
2021-05-06 18:32:01 +0800

    implemented mycontract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-waffle";
 import "tsconfig-paths/register";
-import "dotenv"
+import * as dotenv from "dotenv";
 import { task, HardhatUserConfig } from "hardhat/config";
+
+dotenv.config({ path: __dirname+'/.env' });
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -15,7 +17,9 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 });
 
 const config: HardhatUserConfig = {
-  solidity: "0.7.3",
+  solidity: {
+    compilers: [{version: "0.8.4"}, {version: "0.7.6"}, {version: "0.6.12"}]
+  },
   networks: {
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ROPSTEN_ALCHEMY_API_KEY}`,
