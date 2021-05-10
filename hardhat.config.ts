4278cc11bc80c3896773e3ad5a0fd42d8224a5a2
Ho Chi Ho <hcho1989@gmail.com>
2021-05-10 13:33:46 +0800

    added typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,29 @@
 import { task, HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
-import '@openzeppelin/hardhat-upgrades';
-import 'tsconfig-paths/register';
+import "@openzeppelin/hardhat-upgrades";
+import "tsconfig-paths/register";
+import "@typechain/hardhat";
 import * as dotenv from "dotenv";
-dotenv.config({ path: __dirname+'/.env' });
+dotenv.config({ path: __dirname+"/.env" });
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (args, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(await account.address);
+//   }
+// });
 
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [{version: "0.8.4"}, {version: "0.7.6"}, {version: "0.6.12"}]
+  },
+  typechain: {
+    outDir: "typechain/dist/TypeChain",
+    target: "ethers-v5",
   },
   networks: {
     hardhat: {
